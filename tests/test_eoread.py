e1858836d9ee1f5fd5bff300684347cd66662085
alex lagneau <alex@grisnez.hygeos.com>
2019-07-09 15:43:42 +0200

    adds reflectance, split and merge tests

--- tests/test_eoread.py
+++ tests/test_eoread.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 import pytest
-from eoread.olci import Level1_OLCI, Level2_OLCI
+from eoread.olci import Level1_OLCI, Level2_OLCI, olci_init_spectral
 from eoread.msi import Level1_MSI
 from tests import products as p
 from tests.products import sentinel_product, sample_data_path
@@ -20,6 +20,24 @@ def test_olci_level1(sentinel_product, capsys):
     lon = ds.longitude[100, 100]
     assert ds.eo.contains(lat, lon)
     assert not ds.eo.contains(lat, lon+180)
+
+@pytest.mark.parametrize('product', [p.prod_S3_L1_20190430])
+def test_olci_reflectance(sentinel_product, capsys):
+    ds = Level1_OLCI(sentinel_product)
+    print(ds)
+    ds = ds.eo.sub_rect(55, 56, 18, 19)
+    olci_init_spectral(ds)
+    ds.eo.init()
+
+@pytest.mark.parametrize('product', [p.prod_S3_L1_20190430])
+def test_split_merge(sentinel_product, capsys):
+    ds = Level1_OLCI(sentinel_product)
+    print(ds)
+    ds = ds.eo.sub_rect(55, 56, 18, 19)
+    split = ds.eo.split('Ltoa', out_vars = ['Ltoa_'+str(band) for band in ds.coords['bands'].data], split_axis = 'bands')
+    print(split)
+    merge = split.eo.merge([var for var in split.variables if 'Ltoa_' in var], 'Ltoa', 'bands')
+    print(merge)
 
 
 @pytest.mark.parametrize('product', [p.prod_S3_L2_20190612])
