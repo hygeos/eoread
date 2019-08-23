12183568bda4d31bcca6e8ea54fdddf793571628
François Steinmetz <fs@hygeos.com>
2019-08-23 15:14:06 +0200

    MSI: update tests
    Test subsetting of multiple parameters

--- tests/test_msi.py
+++ tests/test_msi.py
@@ -3,16 +3,18 @@
 
 
 import pytest
+import xarray as xr
 from eoread.msi import Level1_MSI
 from tests import products as p
 from tests.products import sentinel_product, sample_data_path
-from eoread.common import GeoDatasetAccessor
 
 
 
-@pytest.mark.parametrize('product,resolution',
-                         [(p.prod_S2_L1_20190419, res) for res in ['10', '20', '60']])
-def test_msi_merged(sentinel_product, resolution):
+@pytest.mark.parametrize('product,resolution,param',
+                         [(p.prod_S2_L1_20190419, res, param)
+                          for res in ['10', '20', '60']
+                          for param in ['sza', 'vza', 'saa', 'vaa', 'latitude', 'longitude']])
+def test_msi_merged(sentinel_product, resolution, param):
     l1 = Level1_MSI(sentinel_product, resolution)
     print(l1)
     assert 'Rtoa_443' not in l1
@@ -21,6 +23,23 @@ def test_msi_merged(sentinel_product, resolution):
     # Try to access latitude
     sub = l1.sel(rows=slice(50, 100), columns=slice(100, 150))
     sub.latitude.compute()
+
+    # check parameter consistency through windowing
+    xr.testing.assert_allclose(
+        l1[param][1000, 500],
+        l1.isel(rows=slice(1000, None),
+                columns=slice(500, None))[param][0, 0])
+
+    xr.testing.assert_allclose(
+        l1[param][1000:1010, 500:510],
+        l1.isel(rows=slice(1000, None),
+                columns=slice(500, None))[param][:10, :10])
+
+    if resolution == '60':
+        xr.testing.assert_allclose(
+            l1[param].compute()[1000:1010, 500:510],
+            l1.isel(rows=slice(1000, None),
+                    columns=slice(500, None))[param][:10, :10])
 
 
 @pytest.mark.parametrize('product,resolution',
