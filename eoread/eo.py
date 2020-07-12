de8b7fa93dd2ac0c900add5d3c24dab61df38a6c
François Steinmetz <fs@hygeos.com>
2020-07-12 15:37:27 +0200

    Minor fix

--- eoread/eo.py
+++ eoread/eo.py
@@ -101,7 +101,7 @@ def init_geometry(ds, scat_angle=False):
     # scattering angle
     if scat_angle:
         ds['scat_angle'] = scattering_angle(ds.mus, ds.muv, ds.raa)
-        ds['scat_angle'].attrs = 'scattering angle'
+        ds['scat_angle'].attrs['description'] = 'scattering angle'
 
     return ds
 
