cb1c49a14ef67bfbe7322bcd663d5962915b02d5
François Steinmetz <fs@hygeos.com>
2020-08-30 23:13:41 +0200

    make landsat8 tests work without gdal

--- tests/test_landsat8.py
+++ tests/test_landsat8.py
@@ -16,23 +16,29 @@ from eoread.landsat8_oli import LATLON_GDAL, LATLON_NOGDAL, TOA_READ, Level1_L8_
 from . import generic
 from .generic import indices, param, scheduler
 
+try:
+    import gdal
+except ModuleNotFoundError:
+    gdal = None
+
 
 sample_landsat8_oli = p['prod_oli_L1']['path']
 
 @pytest.mark.parametrize('lat_or_lon', ['lat', 'lon'])
 def test_latlon(lat_or_lon):
-    latlon_gdal = LATLON_GDAL(sample_landsat8_oli, lat_or_lon)
-    assert latlon_gdal[:20, :10].shape == (20, 10)
-    assert latlon_gdal[:20:2, :10:2].shape == (10, 5)
-
     latlon_nogdal = LATLON_NOGDAL(sample_landsat8_oli, lat_or_lon)
     assert latlon_nogdal[:20, :10].shape == (20, 10)
     assert latlon_nogdal[:20:2, :10:2].shape == (10, 5)
 
-    assert latlon_gdal.shape == latlon_nogdal.shape
-    np.testing.assert_allclose(
-        latlon_gdal[::100, ::100],
-        latlon_nogdal[::100, ::100])
+    if gdal is not None:
+        latlon_gdal = LATLON_GDAL(sample_landsat8_oli, lat_or_lon)
+        assert latlon_gdal[:20, :10].shape == (20, 10)
+        assert latlon_gdal[:20:2, :10:2].shape == (10, 5)
+
+        assert latlon_gdal.shape == latlon_nogdal.shape
+        np.testing.assert_allclose(
+            latlon_gdal[::100, ::100],
+            latlon_nogdal[::100, ::100])
 
 
 @pytest.mark.parametrize('b', [440, 480, 560, 655, 865, 1375, 1610, 2200])
@@ -59,7 +65,7 @@ def test_main():
     generic.test_main(l1)
 
 
-@pytest.mark.parametrize('use_gdal', [True, False])
+@pytest.mark.parametrize('use_gdal', [False] if (gdal is None) else [True, False])
 def test_read(param, indices, scheduler, use_gdal):
     l1 = Level1_L8_OLI(sample_landsat8_oli, use_gdal=use_gdal)
     eo.init_geometry(l1)
