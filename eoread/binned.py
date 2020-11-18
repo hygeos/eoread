250d7aef4383c2bfa6c0d0626de5bd6ab86fcd62
François Steinmetz <fs@hygeos.com>
2020-11-18 20:13:02 +0100

    Add methods to manage binned products (sinusoidal grid)
    - Read binned products in GSFC format
    - Bin own data for comparison
    - Create 2D maps from binned data

---
+++ eoread/binned.py
@@ -0,0 +1,108 @@
+import xarray as xr
+import numpy as np
+from matplotlib import pyplot as plt
+
+"""
+Interface with binned L3 products (GSFC)
+
+See https://169.154.128.44/docs/technical/ocean_level-3_binned_data_products.pdf
+"""
+
+
+def ncols(neq):
+    """
+    returns an array of the number of valid columns per row in
+    sinusoidal projection with neq bins at equator
+    """
+    return np.round(np.sin(np.linspace(0, 1, neq+1)[1::2]*np.pi)*neq).astype('int')
+
+
+def latlon2bin_sinu(lat, lon, neq, lon0=0.):
+    """
+    lat, lon to bin index in sin grid with neq bins at equator
+    """
+    nc = ncols(neq)
+
+    # index of first bin of each row
+    start_num = np.concatenate(([0], np.cumsum(nc)[:-1]))
+
+    ilat = ((neq/2)*(lat+90.)/180.).astype('int')
+    ilon = np.minimum((nc[ilat]*(lon+180-lon0 % 360)/360.).astype('int'), nc[ilat])
+
+    return ilon + start_num[ilat]
+
+
+class Binner:
+    """
+    Accululate values in a (flattened) sinusoidal grid
+    """
+    def __init__(self, neq):
+        nc = ncols(neq)
+        self.nbins = np.sum(nc)
+        self.sums = np.zeros(self.nbins, dtype='float64')
+        self.counts = np.zeros(self.nbins, dtype='int32')
+        self.neq = neq
+
+    def add(self, values, lat, lon):
+        ibins = latlon2bin_sinu(lat, lon, self.neq)
+        self.sums = np.bincount(
+            ibins,
+            weights=values,
+            minlength=self.nbins)
+        self.counts = np.bincount(
+            ibins,
+            minlength=self.nbins)
+
+    def values(self):
+        return self.sums/self.counts
+
+
+def read_binned(filename, varname, groupname='level-3_binned_data'):
+    """
+    Opens a binned product in sinusoidal projection as a 2-dim array
+
+    returns: data, lat, lon
+    """
+    ds = xr.open_dataset(
+        filename,
+        group=groupname,
+        )
+    neq = 2*len(ds.BinIndex)
+    bin_num = ds.BinList.data['bin_num']
+    extent = ds.BinIndex.data['extent']
+    data = ds[varname].data['sum']/ds.BinList.data['weights']
+
+    return to_2dim(data, neq, bin_num, extent)
+
+
+def to_2dim(data, neq, bin_num=None, extent=None):
+    """
+    Returns a 2-dimensional array from flattened data in sinusoidal grid
+    """
+    H = neq//2
+    W = neq
+    nc = ncols(W)
+    nbins = nc.sum()
+    if bin_num is None:
+        bin_num = np.arange(nbins) + 1
+    if extent is None:
+        extent = nc
+
+    # index of first bin of each row
+    start_num = np.concatenate(([0], np.cumsum(nc)[:-1]))
+    ilat = np.repeat(np.arange(H), extent)
+    icol = bin_num - np.repeat(start_num, extent) - 1
+
+    off = (W-1-np.repeat(nc, extent))//2
+
+    lat_flat = ((ilat+1-.5)*180./H) - 90.
+    lon_flat = (360. * (icol+.5)/np.repeat(nc, extent)) - 180.
+
+    reprojected = np.zeros((H, W)) + np.NaN
+    reprojected[H-1-ilat, icol + off] = data
+    lat = np.zeros((H, W)) + np.NaN
+    lat[H-1-ilat, icol + off] = lat_flat
+    lon = np.zeros((H, W)) + np.NaN
+    lon[H-1-ilat, icol + off] = lon_flat
+
+    return reprojected, lat, lon
---
+++ tests/test_binned.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+import pytest
+import numpy as np
+import xarray as xr
+from matplotlib import pyplot as plt
+from eoread.sample_products import get_sample_products
+from eoread.binned import read_binned, ncols, Binner, to_2dim
+from . import conftest
+
+p = get_sample_products()
+
+products = [
+    (p['prod_MODIS_binned_chl'], 'chl_ocx'),
+]
+
+@pytest.mark.parametrize('product,varname', products)
+def test_read_binned(product, varname, request):
+
+    dat = read_binned(product['path'], varname)[0]
+    dat[np.isnan(dat)] = 0.
+
+    plt.figure()
+    plt.imshow(dat,
+        vmin=0,
+        vmax=dat.mean()*1.4,
+        )
+    plt.colorbar()
+    conftest.savefig(request)
+
+
+@pytest.mark.parametrize('product', products)
+def test_check_ncols(product):
+    ds = xr.open_dataset(
+        product['path'],
+        group='level-3_binned_data',
+        )
+    neq = len(ds.binIndexDim)*2
+
+    start_num = ds.BinIndex.data['start_num']
+    nc = ncols(neq)
+    assert (np.diff(start_num) == nc[:-1]).all()
+
+@pytest.mark.parametrize('product,varname', products)
+def test_consistency(product, varname, request):
+    dat, lat, lon = read_binned(product['path'], varname)
+
+    ok = ~np.isnan(dat)
+    neq = 2*len(xr.open_dataset(product['path'], group='level-3_binned_data').BinIndex)
+
+    binner = Binner(neq)
+    binner.add(dat[ok], lat[ok], lon[ok])
+
+    dat1, lat1, lon1 = to_2dim(binner.values(), neq)
+
+    np.testing.assert_allclose(dat, dat1)
+
+    for (txt, d) in [
+            ('data', dat),
+            ('lat', lat),
+            ('lon', lon),
+            ('data1', dat1),
+            ('lat1', lat1),
+            ('lon1', lon1),
+        ]:
+        plt.figure()
+        plt.imshow(d)
+        plt.colorbar()
+        plt.title(txt)
+        conftest.savefig(request)
