2b40ed4d9ea51dc3163884a7fe975bc780582354
François Steinmetz <fs@hygeos.com>
2023-05-18 18:52:02 +0200

    Update cache module
    Make cachefunc generic, and use it in the other functions specialized
    per file format.

--- eoread/cache.py
+++ eoread/cache.py
@@ -2,83 +2,117 @@ from functools import wraps
 import json
 from pathlib import Path
 import pickle
+from typing import Callable, Optional
 import xarray as xr
 from eoread import eo
+from eoread.fileutils import filegen
 
 
-def cachefunc(cache_file, method='json'):
+def cachefunc(cache_file: Path,
+              reader: Callable,
+              writer: Callable,
+              checker: Optional[Callable] = None,
+              fg_kwargs=None):
     """
-    A decorator that caches the return of a function in a file
-    
-    method: 'json' or 'pickle'
+    A decorator that caches the return of a function in a file, with
+    customizable format
+
+    writer: a function
+        obj = reader(filename)
+    reader: a function
+        writer(filename, obj)
+    checker: a custom function to test the equality of the two objects
+        checker(obj1, obj2)
+        (defaults to ==)
+    fg_kwargs: kwargs passed to filegen (ex: lock_timeout=-1)
     """
     def decorator(f):
         @wraps(f)
         def wrapper(*args, **kwargs):
-            if Path(cache_file).exists():
-                if method == 'json':
-                    with open(cache_file) as fp:
-                        return json.load(fp)
-                elif method == 'pickle':
-                    with open(cache_file, 'rb') as fp:
-                        return pickle.load(fp)
-                else:
-                    raise ValueError
-            else:
+            if not cache_file.exists():
                 result = f(*args, **kwargs)
-                Path(cache_file).parent.mkdir(exist_ok=True, parents=True)
-                if method == 'json':
-                    with open(cache_file, 'w') as fp:
-                        json.dump(result, fp, indent=4)
-                elif method == 'pickle':
-                    with open(cache_file, 'wb') as fp:
-                        pickle.dump(result, fp)
+
+                filegen(**(fg_kwargs or {}))(writer)(cache_file, result)
+            else:
+                result = None
+
+            obj = reader(cache_file)
+
+            if result is not None:
+                # check that the object read back is identical
+                # to the original result (defaults to ==)
+                if checker is None:
+                    assert result == obj
                 else:
-                    raise ValueError
-                return result
+                    assert checker(result, obj)
+
+            return obj
+
         return wrapper
     return decorator
-        
+
+
+def cache_json(cache_file: Path):
+
+    def reader(filename):
+        with open(filename) as fp:
+            return json.load(fp)
+
+    def writer(filename, obj):
+        with open(filename, 'w') as fp:
+            json.dump(obj, fp, indent=4)
+
+    return cachefunc(
+        cache_file,
+        reader=reader,
+        writer=writer,
+    )
+
+
+def cache_pickle(cache_file: Path):
+
+    def reader(filename):
+        with open(filename, 'rb') as fp:
+            return pickle.load(fp)
+
+    def writer(filename, obj):
+        with open(filename, 'wb') as fp:
+            pickle.dump(obj, fp)
+    return cachefunc(
+        cache_file,
+        reader=reader,
+        writer=writer,
+    )
+
 
 def cache_dataset(cache_file,
                   attrs=None,
-                  verbose=True,
                   **kwargs):
     """
     A decorator that caches the dataset returned by a function in a netcdf file
-    
-    The attribute dictionary `attrs` is stored in the file, and verified upon reading
+
+    The attribute dictionary `attrs` is stored in the file, and verified upon
+    reading.
 
     Other kwargs (ex: chunks) are passed to xr.open_dataset
     """
-    def decorator(f):
-        @wraps(f)
-        def wrapper(*args, **wrap_kwargs):
+    def reader(filename):
+        ds = xr.open_dataset(filename, **kwargs)
 
-            if not Path(cache_file).exists():
-                ds = f(*args, **wrap_kwargs)
-                Path(cache_file).parent.mkdir(exist_ok=True, parents=True)
+        # check attributes in loaded file
+        if attrs is not None:
+            for k, v in attrs.items():
+                assert ds.attrs[k] == v, \
+                    f'Error when checking attribute {k}: {ds.attrs[k]} != {v}'
+        return ds
 
-                # store provided attributes in cache file
-                if attrs is not None:
-                    ds.attrs.update(attrs)
+    def writer(filename, ds):
+        if attrs is not None:
+            ds.attrs.update(attrs)
+        eo.to_netcdf(ds, filename=filename)
 
-                # Write `ds` to `cache_file`
-                eo.to_netcdf(ds, filename=cache_file)
-                if verbose:
-                    print(f'Wrote cache file {cache_file}')
-            elif verbose:
-                print(f'Using cache file {cache_file}')
-
-            ds = xr.open_dataset(cache_file, **kwargs)
-
-            # check attributes in loaded file
-            if attrs is not None:
-                for k, v in attrs.items():
-                    assert ds.attrs[k] == v, \
-                        f'Error when checking attribute {k}: {ds.attrs[k]} != {v}'
-                
-            return ds
-        return wrapper
-    return decorator
-    
+    return cachefunc(
+        cache_file,
+        reader=reader,
+        writer=writer,
+    )
--- tests/test_cache.py
+++ tests/test_cache.py
@@ -1,29 +1,36 @@
 from pathlib import Path
 import pytest
-from eoread.cache import cachefunc, cache_dataset
+from eoread.cache import cache_dataset
+from eoread import cache
 from xarray.tutorial import open_dataset
 from tempfile import TemporaryDirectory
 
-@pytest.mark.parametrize('var', [
-    1,
-    'a',
-    [1, 'b', [1, 2]],
+
+@pytest.mark.parametrize('cache_function,var', [
+    (cache.cache_json, 1),
+    (cache.cache_json, 'a'),
+    (cache.cache_json, [1, 'b', [1, 2]]),
+    (cache.cache_pickle, [1, 'b', [1, 2]]),
 ])
-def test_cachefunc(var):
+def test_cachefunc(cache_function, var):
     def my_function():
         return var
     with TemporaryDirectory() as tmpdir:
         cache_file = Path(tmpdir)/'cache.json'
-        cachefunc(cache_file)(my_function)()
-        a = cachefunc(cache_file)(my_function)()
+
+        cache_function(cache_file)(my_function)()
+        a = cache_function(cache_file)(my_function)()
         assert a == my_function()
+    
 
 def test_cache_dataset():
     def my_function():
         return open_dataset('air_temperature')
     with TemporaryDirectory() as tmpdir:
         cache_file = Path(tmpdir)/'cache.nc'
-        cache_dataset(cache_file)(my_function)()
+        cache_dataset(cache_file,
+                      attrs={'a': 1}
+                      )(my_function)()
         a = cache_dataset(
             cache_file,
             chunks={'lat': 10, 'lon': 10},
