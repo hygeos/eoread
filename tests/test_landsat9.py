a0ccd450e56f12b67e331f30734155eb03e4033d
Nathan Capon <nathan.capon@hygeos.com>
2023-12-22 15:19:38 +0100

    fix : Update tests for Landsat reader

--- tests/test_landsat9.py
+++ tests/test_landsat9.py
@@ -12,7 +12,7 @@ from pathlib import Path
 from eoread.sample_products import get_sample_products
 
 from eoread import eo
-from eoread.landsat8_oli import LATLON_GDAL, LATLON_NOGDAL, TOA_READ, Level1_L8_OLI
+from eoread.landsat9_oli import LATLON_GDAL, LATLON_NOGDAL, TOA_READ, Level1_L9_OLI
 
 from . import generic
 from .generic import indices, param, scheduler
@@ -24,16 +24,16 @@ except ModuleNotFoundError:
 
 
 p = get_sample_products()
-sample_landsat8_oli = p['prod_oli_L1']['path']
+sample_landsat9_oli = "/archive2/proj/QTIS_TRISHNA/L8L9/LC09_L1TP_014034_20220618_20230411_02_T1"
 
 @pytest.mark.parametrize('lat_or_lon', ['lat', 'lon'])
 def test_latlon(lat_or_lon):
-    latlon_nogdal = LATLON_NOGDAL(sample_landsat8_oli, lat_or_lon)
+    latlon_nogdal = LATLON_NOGDAL(sample_landsat9_oli, lat_or_lon)
     assert latlon_nogdal[:20, :10].shape == (20, 10)
     assert latlon_nogdal[:20:2, :10:2].shape == (10, 5)
 
     if gdal is not None:
-        latlon_gdal = LATLON_GDAL(sample_landsat8_oli, lat_or_lon)
+        latlon_gdal = LATLON_GDAL(sample_landsat9_oli, lat_or_lon)
         assert latlon_gdal[:20, :10].shape == (20, 10)
         assert latlon_gdal[:20:2, :10:2].shape == (10, 5)
 
@@ -45,14 +45,14 @@ def test_latlon(lat_or_lon):
 
 @pytest.mark.parametrize('b', [440, 480, 560, 655, 865, 1375, 1610, 2200])
 def test_toa_read(b):
-    r = TOA_READ(b, sample_landsat8_oli)
+    r = TOA_READ(b, sample_landsat9_oli)
     assert r[5000:5050, 4000:4040].shape == (50, 40)
     assert r[5000:5050:2, 4000:4040:2].shape == (25, 20)
 
 
 @pytest.mark.parametrize('split', [True, False])
 def test_instantiate(split):
-    l1 = Level1_L8_OLI(sample_landsat8_oli, split=split)
+    l1 = Level1_L9_OLI(sample_landsat9_oli, split=split)
 
     if split:
         assert 'Rtoa' not in l1
@@ -62,20 +62,17 @@ def test_instantiate(split):
         assert 'Rtoa_440' not in l1
 
 
-@pytest.mark.parametrize('path, angle', [
-    (sample_landsat8_oli, True),
-    (Path('/archive2/proj/QTIS_TRISHNA/Brockmann_data/L1_L8/LC81960302014022LGN00'), False)])
-def test_main(path, angle):
-    l1 = Level1_L8_OLI(path, angle_data=angle)
-    generic.test_main(l1, angle)
+def test_main():
+    l1 = Level1_L9_OLI(sample_landsat9_oli)
+    generic.test_main(l1)
 
 @pytest.mark.parametrize('use_gdal', [False] if (gdal is None) else [True, False])
 def test_read(param, indices, scheduler, use_gdal):
-    l1 = Level1_L8_OLI(sample_landsat8_oli, use_gdal=use_gdal)
+    l1 = Level1_L9_OLI(sample_landsat9_oli, use_gdal=use_gdal)
     eo.init_geometry(l1)
     generic.test_read(l1, param, indices, scheduler)
 
 
 def test_subset():
-    l1 = Level1_L8_OLI(sample_landsat8_oli)
+    l1 = Level1_L9_OLI(sample_landsat9_oli)
     generic.test_subset(l1)
