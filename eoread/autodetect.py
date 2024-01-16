2c06fdabbf9deb5e82702d4607df209bd6e2cde1
François Steinmetz <fs@hygeos.com>
2024-01-16 09:59:34 +0000

    Add L1 and L2 autodetection module
    Open Level1 and Level2 by autodetecting the sensor type.

---
+++ eoread/autodetect.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+Generic product loader with autodetection
+"""
+
+from pathlib import Path
+import xarray as xr
+import re
+
+
+def Level1(path: Path, **kwargs) -> xr.Dataset:
+    if re.match("^S3[AB]_OL_1_EFR____.*", path.name):
+        from eoread.olci import Level1_OLCI
+
+        return Level1_OLCI(path, **kwargs)
+
+    else:
+        raise ValueError(f"Could not detect Level1 type for {path.name}")
+
+
+def Level2(path: Path, **kwargs) -> xr.Dataset:
+    if re.match("^S3[AB]_OL_2_WFR____.*", path.name):
+        from eoread.olci import Level2_OLCI
+
+        return Level2_OLCI(path, **kwargs)
+
+    else:
+        raise ValueError(f"Could not detect Level1 type for {path.name}")
