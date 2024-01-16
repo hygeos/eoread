ff24306162f13ed4cff68341bc141c1bf04aaaca
François Steinmetz <fs@hygeos.com>
2024-01-16 10:00:37 +0000

    Update test_olci with new samples structure

--- tests/test_olci.py
+++ tests/test_olci.py
@@ -3,21 +3,21 @@
 
 import pytest
 from matplotlib import pyplot as plt
-from eoread.sample_products import product_getter
-from eoread.olci import Level1_OLCI, Level2_OLCI, olci_init_spectral
+from eoread.autodetect import Level1, Level2
 from eoread.olci import get_valid_l2_pixels
+from eoread import olci
 from eoread import eo
-import numpy as np
 from . import generic
-from .generic import param, indices, scheduler
+from .generic import param, indices, scheduler  # noqa (fixtures)
 from .conftest import savefig
 
 
-product = pytest.fixture(params=['prod_S3_L1_20190430'])(product_getter)
+olci_level1 = pytest.fixture(lambda: olci.get_sample('level1_fr'))
+olci_level2 = pytest.fixture(lambda: olci.get_sample('level2_fr'))
 
 
-def test_olci_level1(product):
-    ds = Level1_OLCI(product['path'])
+def test_olci_level1(olci_level1):
+    ds = Level1(olci_level1)
 
     ds = eo.chunk(ds, bands=-1)
     ds.chunks    # check that it returned valid chunks
@@ -33,8 +33,8 @@ def test_olci_level1(product):
     assert 'total_columnar_water_vapour' in ds
 
 
-def test_split_merge(product):
-    ds = Level1_OLCI(product['path'])
+def test_split_merge(olci_level1):
+    ds = Level1(olci_level1)
     print(ds)
     ds = eo.sub_rect(ds, 55, 56, 18, 19)
     split = eo.split(ds, 'bands')
@@ -43,40 +43,38 @@ def test_split_merge(product):
     print(merge)
 
 
-def test_sub_pt(product):
-    ds = Level1_OLCI(product['path'])
+def test_sub_pt(olci_level1):
+    ds = Level1(olci_level1)
     lat0 = ds.latitude[500, 500]
     lon0 = ds.longitude[500, 500]
     eo.sub_pt(ds, lat0, lon0, 3)
 
 
-@pytest.mark.parametrize('product', ['prod_S3_L2_20190612'], indirect=True)
-def test_olci_level2(product):
-    l2 = Level2_OLCI(product['path'])
+def test_olci_level2(olci_level2):
+    l2 = Level2(olci_level2)
     print(l2)
 
 
-@pytest.mark.parametrize('product', ['prod_S3_L2_20190612'], indirect=True)
-def test_olci_level2_flags(product):
-    l2 = Level2_OLCI(product['path'])
+def test_olci_level2_flags(olci_level2):
+    l2 = Level2(olci_level2)
 
     eo.getflags(l2.wqsf)
     get_valid_l2_pixels(l2.wqsf)
 
 
-def test_main(product):
-    ds = Level1_OLCI(product['path'])
+def test_main(olci_level1):
+    ds = Level1(olci_level1)
     eo.init_Rtoa(ds)
     generic.test_main(ds)
 
-def test_read(product, param, indices, scheduler):
-    ds = Level1_OLCI(product['path'])
+def test_read(olci_level1, param, indices, scheduler):
+    ds = Level1(olci_level1)
     eo.init_Rtoa(ds)
     generic.test_read(ds, param, indices, scheduler)
 
 
-def test_subset(product):
-    ds = Level1_OLCI(product['path'])
+def test_subset(olci_level1):
+    ds = Level1(olci_level1)
     generic.test_subset(ds)
 
 @pytest.mark.parametrize('s', [
@@ -84,12 +82,12 @@ def test_subset(product):
     slice(None, None, 10),
     ])
 @pytest.mark.parametrize('interp', ['legacy', 'linear', 'atan2'])
-def test_preview(product, param, request, s, interp):
+def test_preview(olci_level1, param, request, s, interp):
     """
     Generate browses of various products
     """
     plt.figure()
-    l1 = Level1_OLCI(product['path'], interp_angles=interp)
+    l1 = Level1(olci_level1, interp_angles=interp)
     eo.init_Rtoa(l1)
     if l1[param].ndim == 2:
         plt.imshow(l1[param][s, s])
@@ -99,10 +97,10 @@ def test_preview(product, param, request, s, interp):
     savefig(request)
 
 
-def test_flag(product):
+def test_flag(olci_level1):
     """
     Check that flags are properly raised
     """
-    l1 = Level1_OLCI(product['path'])
+    l1 = Level1(olci_level1)
     assert (l1.quality_flags > 0).any()
     assert (l1.flags > 0).any()
