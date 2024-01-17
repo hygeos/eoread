0041416b3a3465a23e14fac12b8913acad5d703d
François Steinmetz <fs@hygeos.com>
2024-01-17 12:59:57 +0000

    Add graphics.py module with plot_srf
    Supports testing

---
+++ eoread/utils/graphics.py
@@ -0,0 +1,18 @@
+from matplotlib import pyplot as plt
+import xarray as xr
+
+
+def plot_srf(srf: xr.Dataset):
+    """
+    Plot a SRF Dataset
+    """
+    plt.figure()
+    for iband in sorted(srf.data_vars):
+        srf[iband].plot(label=iband)
+        for coord in srf[iband].coords:
+            assert "units" in srf[coord].attrs
+    plt.title(srf.desc)
+    plt.legend(bbox_to_anchor=(1.04, 1), loc="upper left")
+    plt.xlabel("wavelength")
+    plt.ylabel("SRF")
+    plt.grid(True)
