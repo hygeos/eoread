055fbe6fd1adcfd87fe4001d639ba4fdcca8f086
François Steinmetz <fs@hygeos.com>
2024-02-12 10:36:42 +0100

    Implement interpolate.selinterp
    Replaces interp, but:
    - With the ability to apply either interp or sel
    - With a cleaner interface, similar to ds.interp and ds.sel

--- eoread/utils/interpolate.py
+++ eoread/utils/interpolate.py
@@ -1,7 +1,92 @@
-from typing import Optional
+from typing import Literal, Optional
 
 import warnings
 import xarray as xr
+
+
+def selinterp(
+    da: xr.DataArray,
+    *,
+    method: Literal["interp", "nearest"],
+    template: Optional[xr.DataArray] = None,
+    **kwargs: xr.DataArray,
+) -> xr.DataArray:
+    """
+    Interpolation (or selection) of xarray DataArray `da` along dimensions
+    provided as kwargs.
+
+    xarray's `interp` and `sel` methods do not work efficiently on dask-based
+    coordinates arrays (full arrays are computed); this function is
+    designed to work around this.
+
+    `method`:
+        "interp" to apply da.interp
+        "nearest" to apply da.sel with method="nearest"
+
+    `template`: the DataArray template for the output of this function.
+    By default, use first kwarg.
+
+    Example:
+        selinterp(auxdata,
+                  method='interp',
+                  lat=ds.latitude,
+                  lon=ds.longitude)
+
+    This interpolates `auxdata` along dimensions 'lat' and 'lon', by chunks,
+    with values defined in ds.latitude and ds.longitude.
+    """
+
+    # Check that aux is not dask-based
+    assert (da.chunks is None) or (len(da.chunks) == 0), \
+        'Input DataArray should not be dask-based'
+
+    first_dim = list(kwargs.values())[0]
+    template = template or first_dim.reset_coords(drop=True).rename(da.name).astype(
+        {
+            "interp": "float64",
+            "nearest": da.dtype,
+        }[method]
+    )
+
+    # update the template with input attributes
+    template.attrs = da.attrs
+
+    func = {
+        "interp": interp_chunk,
+        "nearest": sel_chunk,
+    }[method]
+
+    interpolated = xr.map_blocks(
+        func,
+        xr.Dataset(kwargs),
+        template=template,
+        kwargs={"aux": da, "dims": {k: v.name for k, v in kwargs.items()}},
+    )
+
+    return interpolated
+
+
+def interp_chunk(ds, aux, dims):
+    """
+    Apply da.interp to a single chunk
+    """
+    with warnings.catch_warnings():
+        warnings.simplefilter("ignore", category=RuntimeWarning)
+        return aux.interp(
+            {k: ds[v]
+                for (k, v) in dims.items()
+                }).reset_coords(drop=True)
+
+
+def sel_chunk(ds, aux, dims):
+    """
+    da.sel of a single chunk
+    """
+    with warnings.catch_warnings():
+        warnings.simplefilter("ignore", category=RuntimeWarning)
+        return aux.sel(
+            {k: ds[v] for (k, v) in dims.items()}, method="nearest"
+        ).reset_coords(drop=True)
 
 
 def interp(aux: xr.DataArray,
@@ -29,6 +114,9 @@ def interp(aux: xr.DataArray,
     This interpolates `auxdata` along dimensions 'lat' and 'lon', by chunks,
     with values defined in ds['latitude'] and ds['longitude'].
     """
+    warnings.warn(
+        "Deprecated function: use function `selinterp` instead", DeprecationWarning
+    )
     def interp_chunk(ds):
         """
         Interpolation of a single chunk
