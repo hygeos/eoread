474f94d5ee799268e0fef7d7f781511ab79b7048
François Steinmetz <fs@hygeos.com>
2024-06-14 14:31:17 +0200

    Function caching : allow checking input parameters

--- eoread/utils/cache.py
+++ eoread/utils/cache.py
@@ -6,7 +6,7 @@ import xarray as xr
 from functools import wraps
 from pathlib import Path
 from tempfile import TemporaryDirectory
-from typing import Callable, Optional
+from typing import Callable, Literal, Optional
 from pandas.testing import assert_frame_equal
 
 from .save import to_netcdf
@@ -16,21 +16,32 @@ from .fileutils import filegen, safe_move
 def cachefunc(cache_file: Path|str,
               reader: Callable,
               writer: Callable,
-              checker: Optional[Callable] = None,
+              check_in: Optional[Callable] = None,
+              check_out: Optional[Callable] = None,
               fg_kwargs=None):
     """
     A decorator that caches the return of a function in a file, with
     customizable format
 
-    reader: a function
-        obj = reader(filename)
-    writer: a function
-        writer(filename, obj)
-    checker: a custom function to test the equality of the two objects
-        checker(obj1, obj2)
+    reader: a function that reads inputs/output from the cache file
+        reader(filename) -> {'output': ..., 'input': ...}
+
+    writer: a function that writes the inputs/output to the cache file
+        writer(filename, output, input_args, input_kwargs)
+
+    check_in: a custom function to test the equality of the inputs
+        checker(obj1, obj2) -> bool
+        (defaults to None -> no checking)
+    
+    check_out: a custom function to test the equality of the outputs
+        checker(obj1, obj2) -> bool
         (defaults to ==)
+
     fg_kwargs: kwargs passed to filegen (ex: lock_timeout=-1)
     """
+    # default input/output checkers
+    check_out = check_out or (lambda x, y: x == y)
+
     cache_file = Path(cache_file)
     def decorator(f):
         @wraps(f)
@@ -43,69 +54,103 @@ def cachefunc(cache_file: Path|str,
                                         prefix='cachefunc_') as tmpdir:
                     cache_file_tmp = Path(tmpdir)/cache_file.name
 
-                    # write the temporary file
-                    filegen(**(fg_kwargs or {}))(writer)(cache_file_tmp, result)
+                    # write the temporary cache file
+                    filegen(**(fg_kwargs or {}))(writer)(
+                        cache_file_tmp, result, args, kwargs
+                    )
 
-                    # check that the object read back is identical
-                    # to the original result (defaults to ==)
-                    obj = reader(cache_file_tmp)
-                    
-                    if checker is None:
-                        assert result == obj
-                    else:
-                        assert checker(result, obj)
-                    
+                    # check that the output read back is identical
+                    # to the original output (defaults to ==)
+                    chk = reader(cache_file_tmp)
+                    assert 'output' in chk
+
+                    assert check_out(result, chk['output'])
+
                     # check successful: move the file
                     safe_move(cache_file_tmp, cache_file)
                     assert cache_file.exists()
-                    
-                    return obj
+
+                    return chk['output']
             else:
-                return reader(cache_file)
+                content = reader(cache_file)
+
+                # check function inputs
+                if check_in:
+                    with TemporaryDirectory() as tmpdir:
+                        # write and read back the inputs for checking
+                        tmpchk = Path(tmpdir)/'tmp_check'
+                        writer(tmpchk, None, args, kwargs)
+                        chk = reader(tmpchk)
+                        assert check_in(content['input'], chk['input'])
+
+                return content['output']
 
         return wrapper
     return decorator
 
 
-def cache_dataframe(cache_file: Path|str):
+def cache_dataframe(cache_file: Path | str):
     return cachefunc(
         cache_file,
-        writer=lambda filename, df: df.to_csv(filename, index=False),
-        reader=lambda filename: pd.read_csv(filename, parse_dates=['time']),
-        checker=lambda x, y: assert_frame_equal,
+        writer=lambda filename, df, args, kwargs: df.to_csv(filename, index=False),
+        reader=lambda filename: {"output": pd.read_csv(filename, parse_dates=["time"])},
+        check_out=lambda x, y: assert_frame_equal,
     )
 
 
-def cache_json(cache_file: Path|str):
+def cache_json(
+    cache_file: Path | str,
+    inputs: Literal["check", "store", "ignore"] = "check",
+):
+    """
+    A decorator that caches the result of a function to a json file.
+
+    inputs:
+        "check" [default]: store and check the function inputs
+        "store": store but don't check the function inputs
+        "ignore": ignore the function inputs
+    """
 
     def reader(filename):
         with open(filename) as fp:
             return json.load(fp)
 
-    def writer(filename, obj):
+    def writer(filename, output, input_args, input_kwargs):
         with open(filename, 'w') as fp:
-            json.dump(obj, fp, indent=4)
+            content = {}
+            if inputs in ['store', 'check']:
+                content['input'] = {'args': input_args, 'kwargs': input_kwargs}
+            content['output'] = output
+            json.dump(content, fp, indent=4, default=str)
 
     return cachefunc(
         cache_file,
         reader=reader,
         writer=writer,
+        check_in=(lambda x, y : x == y) if (inputs == 'check') else None,
     )
 
 
-def cache_pickle(cache_file: Path|str):
+def cache_pickle(
+    cache_file: Path | str,
+    inputs: Literal["check", "store", "ignore"] = "check",
+):
 
     def reader(filename):
         with open(filename, 'rb') as fp:
             return pickle.load(fp)
 
-    def writer(filename, obj):
+    def writer(filename, output, input_args, input_kwargs):
         with open(filename, 'wb') as fp:
-            pickle.dump(obj, fp)
+            content = {}
+            content['input'] = {'args': input_args, 'kwargs': input_kwargs}
+            content['output'] = output
+            pickle.dump(content, fp)
     return cachefunc(
         cache_file,
         reader=reader,
         writer=writer,
+        check_in=(lambda x, y : x == y) if (inputs == 'check') else None,
     )
 
 
@@ -128,12 +173,12 @@ def cache_dataset(cache_file: Path|str,
             for k, v in attrs.items():
                 assert ds.attrs[k] == v, \
                     f'Error when checking attribute {k}: {ds.attrs[k]} != {v}'
-        return ds
+        return {'output': ds}
 
-    def writer(filename, ds):
+    def writer(filename, ds_out, input_args, input_kwargs):
         if attrs is not None:
-            ds.attrs.update(attrs)
-        to_netcdf(ds, filename=filename)
+            ds_out.attrs.update(attrs)
+        to_netcdf(ds_out, filename=filename)
 
     return cachefunc(
         cache_file,
--- tests/test_cache.py
+++ tests/test_cache.py
@@ -13,13 +13,18 @@ from tempfile import TemporaryDirectory
     (cache.cache_pickle, [1, 'b', [1, 2]]),
 ])
 def test_cachefunc(cache_function, var):
-    def my_function():
+    def my_function(*args):
         return var
     with TemporaryDirectory() as tmpdir:
         cache_file = Path(tmpdir)/'cache.json'
 
         cache_function(cache_file)(my_function)()
         a = cache_function(cache_file)(my_function)()
+        
+        with pytest.raises(AssertionError):
+            # raise an error when called with the wrong arguments
+            cache_function(cache_file)(my_function)(0)
+
         assert a == my_function()
     
 
